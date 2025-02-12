4edf744104181d2867bcbe33aba7b0a40d80b24f
Sudipta Nayak <sudipta1@users.noreply.github.com>
2025-02-12 15:58:36 +0530

    Subnet added

--- main.tf
+++ main.tf
@@ -17,7 +17,13 @@ resource "aws_s3_bucket" "ex" {
 }
 
 resource "aws_vpc" "project1" {
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id = aws_vpc.project1.id
+  cidr_block = "10.0.0.1/24"
+  availability_zone = "us-west-1a"
 }
